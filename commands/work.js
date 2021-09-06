a914326e0b925843cc3c4800b8ba76e346123ddf
4techguns <4techgunsinbox@gmail.com>
2021-09-06 16:56:10 -0230

    do some rounding lol

--- commands/work.js
+++ commands/work.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('work')
 		.setDescription('work so you can get some money'),
 	async execute(interaction) {
-        let rand = Math.random() * (10000 - 5000) + 5000
+        let rand = Math.round(Math.random() * (10000 - 5000) + 5000)
 		await interaction.reply(`
             ***DISCLAIMER: THIS COMMAND IS A TEST IT DOESNT ACTUALLY DO MUCH OTHER THAN GIVE YOU A RANDOM NUMBER***
             you got ${rand} coins
