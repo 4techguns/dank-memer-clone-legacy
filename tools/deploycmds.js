7e92d5ba239228af2e2bee5e4830de9fbd6a1d30
4techguns <4techgunsinbox@gmail.com>
2021-09-06 16:52:20 -0230

    oopsies

--- tools/deploycmds.js
+++ tools/deploycmds.js
@@ -1,10 +1,11 @@
-const fs = require('fs')
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId } = require('../config.json');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('../config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,6 +14,7 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
+
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
