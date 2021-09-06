378ada5d16f80d87e92eb6579c8a30a0a69ba958
4techguns <4techgunsinbox@gmail.com>
2021-09-06 16:54:09 -0230

    fine node if thats what you want

--- deploycmds.js
+++ deploycmds.js
@@ -5,10 +5,10 @@ dotenv.config();
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId } = require('../config.json');
+const { clientId, guildId } = require('./config.json');
 
 const commands = [];
-const commandFiles = fs.readdirSync('../commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
