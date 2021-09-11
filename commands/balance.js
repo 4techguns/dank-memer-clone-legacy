176abfe51deb8dece9dfcef9f47d7833c8276fe7
4techguns <4techgunsinbox@gmail.com>
2021-09-11 21:02:36 -0230

    fixed balance command

--- commands/balance.js
+++ commands/balance.js
@@ -5,19 +5,30 @@ const Keyv = require('keyv');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bal')
-		.setDescription('check ur coins'),
+		.setDescription('check ur coins')
+        .addMentionableOption(option =>
+            option.setName('user')
+                .setDescription('the user you want to view their coins on')
+                .setRequired(false)),
 	async execute(interaction) {
 		await interaction.deferReply();
-		const keyv = new Keyv('sqlite://../botDB.sqlite');
+
+        let user = await interaction.options.getMentionable('user', false);
+
+        const keyv = new Keyv('sqlite://../botDB.sqlite');
+
+        let getthing = (!user)
+        ? await keyv.get(interaction.user.id)
+        : await keyv.get(user.id);
 
         let embBase = new MessageEmbed()
             .setFooter('calculayshuns')
             .setTitle('coins')
-            .setDescription('<a:loadin:886214245845458944> ples w8');
+            .setDescription('<a:loadin:886214245845458944>');
 
         await interaction.editReply({embeds: [embBase]});
 
-        if ((await keyv.get(interaction.user.id)) < 10000) {
+        if ((getthing) < 30000 || (getthing) == undefined) {
             await embBase.setFooter('broke 👎😹👎')
         } else {
             await embBase.setFooter('pogr 😎👌')
@@ -25,7 +36,7 @@ module.exports = {
 
         await interaction.editReply({embeds: [embBase]});
 
-        await embBase.setDescription(`:yellow_circle: you have ${await keyv.get(interaction.user.id)} coins`);
+        await embBase.setDescription(`:yellow_circle: ${getthing}`);
 
 		await interaction.editReply({embeds: [embBase]});
 	},
