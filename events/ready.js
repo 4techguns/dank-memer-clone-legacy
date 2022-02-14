daab23724b00ed2baf44b1937c6b6f0fb4408027
4techguns <4techgunsalt@gmail.com>
2022-02-14 17:43:11 +0000

    add legacy message

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,13 @@ module.exports = {
 	once: true,
 	execute() {
 		console.timeEnd('bot start');
-        console.log(`hello`);
+        	console.log(`hello`);
+		client.user.setPresence({
+        		status: "online",  // You can show online, idle... Do not disturb is dnd
+        		game: {
+            			name: "LEGACY VERSION; WILL BE MIGRATED SOON",  // The message shown
+            			type: "PLAYING" // PLAYING, WATCHING, LISTENING, STREAMING,
+        		}
+    		});
 	},
 };
